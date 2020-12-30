64ee6137abe2d51587e62fabe332e1a7aafc12ba
wangchi <15256544909@163.com>
2020-12-30 11:27:22 +0800

    修改host

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,2 +1,3 @@
 // export const devuri = "http://192.168.31.104:7000/api/base";
 export const devuri = "http://192.168.43.11:7000/api/base";
+// export const devuri = "http://192.168.43.11:7000/api/base";
