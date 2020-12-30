77cb1d7a5b5f90222908a9bd0a8a2bde50023c73
wangchi <15256544909@163.com>
2020-12-30 17:04:22 +0800

    status撒旦飞洒

--- src/routes/router.config.js
+++ src/routes/router.config.js
@@ -7,10 +7,10 @@ import Date from '../pages/date/index.jsx';
 
 export const routerConfig = [
     {
-        path:'/home',
+        path:'/home', // 首页
         component: Home
     },{
-        path:'/kind',
+        path:'/kind', // 发范德萨分
         component: Kind
     },{
         path:'/cart',
