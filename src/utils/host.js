9118ed860a5b86eab3ee336eea2e30aa12a10b78
wangchi <15256544909@163.com>
2020-12-30 15:50:27 +0800

    删除注释

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,3 +1,2 @@
 // export const devuri = "http://192.168.31.104:7000/api/base";
 export const devuri = "http://192.168.43.11:7000/api/base";
-// export const devuri = "http://192.168.43.11:7000/api/base";
