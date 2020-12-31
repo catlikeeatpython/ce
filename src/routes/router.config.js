c8c91bf5cd573274cd54eb34a84378a51f96a8bb
wangchi <15256544909@163.com>
2020-12-31 13:40:37 +0800

    sdfsdfsdf

--- src/routes/router.config.js
+++ src/routes/router.config.js
@@ -1,25 +1,22 @@
-
 import Home from '../pages/home/index.jsx';
 import Kind from '../pages/kind/index.jsx';
 import Cart from '../pages/cart/index.jsx';
 import SortTable from '../pages/sortables/index.jsx';
 import Date from '../pages/date/index.jsx';
 
-export const routerConfig = [
-    {
-        path:'/home', // 首页asdfasfdasdf
-        component: Home
-    },{
-        path:'/kind', // 发范德萨分asfasfasf
-        component: Kind
-    },{
-        path:'/cart',
-        component: Cart
-    },{
-        path:'/sortTable',
-        component: SortTable
-    },{
-        path:'/date',
-        component: Date
-    }
-];
+export const routerConfig = [{
+    path: '/home', // 首页asdfasfdasdf
+    component: Home
+}, {
+    path: '/kind', // 发范德萨分asfasfasf
+    component: Kind
+}, {
+    path: '/cart',
+    component: Cart
+}, {
+    path: '/sortTable',
+    component: SortTable
+}, {
+    path: '/date',
+    component: Date
+}];
