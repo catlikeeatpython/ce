94ca4672ec6072f7a7d99cbbff779883512f54cc
wangchi <15256544909@163.com>
2020-12-31 10:56:48 +0800

    sdfsdf

--- src/layout/App.jsx
+++ src/layout/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route,} from 'react-router-dom';
-// import Home from '../page/home/index.jsx';
 import {routerConfig} from '../routes/router.config.js'
 
 
